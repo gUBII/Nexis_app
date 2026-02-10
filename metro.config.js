af623da66e5496c92ad37ccbb951e2f9c37d7fbf
Farhan <34171259+gUBII@users.noreply.github.com>
2026-02-10 12:04:59 +1100

    chore: update axios version to 1.13.5 and add @react-native/metro-config dependency

--- metro.config.js
+++ metro.config.js
@@ -6,10 +6,20 @@
  */
 
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {resolve} = require('metro-resolver');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 const config = {
+  resolver: {
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName === 'axios') {
+        return resolve(context, 'axios/dist/browser/axios.cjs', platform);
+      }
+
+      return resolve(context, moduleName, platform);
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
